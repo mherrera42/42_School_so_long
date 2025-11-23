741c297df14947114db3face1769e6988a8aeda9
LittleBug  🐞 <mherrera@student.42malaga.com>
2025-11-24 00:36:51 +0100

    Changes in the .h

---
+++ includes/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mherrera <mherrera@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 17:14:44 by mherrera          #+#    #+#             */
+/*   Updated: 2025/06/18 16:42:27 by mherrera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
--- includes/so_long.h
+++ includes/so_long.h
@@ -3,30 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mherrera <mherrera@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: mherrera <mherrera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 16:33:03 by mherrera          #+#    #+#             */
-/*   Updated: 2025/11/11 18:24:00 by mherrera         ###   ########.fr       */
+/*   Updated: 2025/11/23 18:02:10 by mherrera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
+
 # define WIDTH 1920
 # define HEIGHT 1080
+# define TILE_SIZE 64
+
 # include <fcntl.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include "MLX42/include/MLX42/MLX42.h"
+# include "get_next_line.h"
 
-//Estructura juego
+//game struct
 typedef struct  s_game
 {
 	mlx_t		*mlx;
 	mlx_image_t	*player;
+	mlx_image_t *floor;
+	mlx_image_t *walls;
+	mlx_image_t *collect;
+	mlx_image_t *exit;
 } t_game;
 
-//Estructura mapa
+//map struct
 typedef struct s_map
 {
 	char	**map; //Matriz en la que se guardará el mapa
@@ -39,13 +47,24 @@ typedef struct s_map
 	int		collectibles; //Nº de coleccionables
 }	t_map;
 
-//Funciones auxiliares de utilidad
+//main.c
+int		error_msg(char *msg, int fd);
+void	init_game(t_game *game)
+void	keyhook(mlx_key_data_t keydata, void *param);
+
+//utils.c
 size_t	ft_strlen(const char *s);
-void	ft_putstr_fd(char *s, int fd);
+//void	ft_putstr_fd(char *s, int fd);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
-//Funciones de validación del mapa
+//validate_map.c
 int		check_extension(char *filename);
+
+//render_map.c
+
+//textures.c
+void	load_texture(t_game *game, mlx_image_t **img, char *path, int x, int y);
+void	init_textures(t_game *game);
 
 #endif
